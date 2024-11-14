d9d19fcf7ef925944931b1dcf18df5b1e521d968
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-11-14 22:07:40 +0200

    fix: caching issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "@/app/validationSchemas";
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issueId = params.id;
+  const body = await req.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const isExist = await prisma.issue.findUnique({
+    where: { id: parseInt(issueId) },
+  });
+
+  if (!isExist)
+    return NextResponse.json({ error: "Issue nor found." }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: parseInt(issueId) },
+    data: body,
+  });
+
+  return NextResponse.json(updatedIssue);
+}
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ErrorMessage, Spinner } from "@/app/components";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
@@ -16,7 +16,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
@@ -25,15 +25,20 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<IssueFormData>({ resolver: zodResolver(createIssueSchema) });
+  } = useForm<IssueFormData>({ resolver: zodResolver(issueSchema) });
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) {
+        await axios.patch(`/api/issues/${issue.id}`, data);
+      } else {
+        await axios.post("/api/issues", data);
+      }
       router.push("/issues");
+      router.refresh();
     } catch (e: any) {
       setSubmitting(false);
       setError(e.message);
