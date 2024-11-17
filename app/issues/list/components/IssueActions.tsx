c9ba296a326372539096152e21d6d88f36378996
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-11-17 23:00:27 +0200

    fix: add new issue if user authenticated

--- app/issues/list/components/IssueActions.tsx
+++ app/issues/list/components/IssueActions.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import { Button, Flex } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
+  const { status } = useSession();
+
   return (
     <Flex justify="between">
       <IssueStatusFilter />
       <Button>
-        <Link href="/issues/new">New Issue</Link>
+        <Link
+          href={
+            status === "unauthenticated" ? "/api/auth/signin" : "/issues/new"
+          }
+        >
+          New Issue
+        </Link>
       </Button>
     </Flex>
   );
